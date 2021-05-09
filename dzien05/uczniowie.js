044a43b2881f850c9ba0d3db7f675e7a5d9f878a
Michal <gebski.michal@gmail.com>
2021-05-09 10:26:00 +0200

    zadania

---
+++ dzien05/uczniowie.js
@@ -0,0 +1,92 @@
+var uczniowie = [
+    {
+      "imie": "Damian",
+      "nazwisko": "I.",
+      "oceny": "6 4 4 6 1 6 1 5 2 5 3 5"
+    },
+    {
+      "imie": "Maciej",
+      "nazwisko": "A.",
+      "oceny": "3 2 3 3 2 3 4 4 2 2 3 3"
+    },
+    {
+      "imie": "Bonawentura",
+      "nazwisko": "P.",
+      "oceny": "5 5 6 6 6 6 4 3 4 3 6 6"
+    },
+    {
+      "imie": "Marta",
+      "nazwisko": "U.",
+      "oceny": "4 4 1 3 1 1 5 4 1 5 1 5"
+    },
+    {
+      "imie": "Marek",
+      "nazwisko": "P.",
+      "oceny": "2 1 3 5 3 2 1 2 3 3 3 2"
+    },
+    {
+      "imie": "Władysław",
+      "nazwisko": "U.",
+      "oceny": "4 3 2 3 4 2 2 3 4 5 2 6"
+    },
+    {
+      "imie": "Maksymilian",
+      "nazwisko": "L.",
+      "oceny": "4 4 6 4 6 5 5 6 4 3 3 4"
+    },
+    {
+      "imie": "Bonawentura",
+      "nazwisko": "P.",
+      "oceny": "1 4 4 3 4 1 3 4 2 1 3 2"
+    },
+    {
+      "imie": "Barbara",
+      "nazwisko": "O.",
+      "oceny": "2 2 2 4 3 4 3 2 2 2 2 4"
+    },
+    {
+      "imie": "Barbara",
+      "nazwisko": "T.",
+      "oceny": "4 3 4 3 3 3 4 4 3 4 4 4"
+    },
+    {
+      "imie": "Michał",
+      "nazwisko": "O.",
+      "oceny": "3 6 2 2 1 3 4 5 2 3 6 1"
+    },
+    {
+      "imie": "Marta",
+      "nazwisko": "Z.",
+      "oceny": "6 3 4 4 4 6 3 3 5 3 6 3"
+    },
+    {
+      "imie": "Barbara",
+      "nazwisko": "Q.",
+      "oceny": "2 5 2 3 4 6 3 5 5 4 6 4"
+    },
+    {
+      "imie": "Damian",
+      "nazwisko": "V.",
+      "oceny": "5 3 3 4 3 4 3 5 3 5 4 5"
+    },
+    {
+      "imie": "Filip",
+      "nazwisko": "X.",
+      "oceny": "3 4 3 3 3 3 4 2 2 2 4 3"
+    },
+    {
+      "imie": "Damian",
+      "nazwisko": "U.",
+      "oceny": "4 3 5 5 3 5 6 6 4 2 6 3"
+    },
+    {
+      "imie": "Bonawentura",
+      "nazwisko": "V.",
+      "oceny": "6 5 4 4 6 6 3 4 3 5 6 6"
+    },
+    {
+      "imie": "Marek",
+      "nazwisko": "O.",
+      "oceny": "2 5 5 5 4 1 1 6 4 4 3 2"
+    }
+  ]
